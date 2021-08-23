7f8b202587a2660416f099d3050c0cee82eb5b26
sudo-saksham <sakshamgupta391@gmail.com>
2021-08-23 09:43:30 +0530

    fixed dynamic time zone visibility issue.

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:world_timer/pages/loading_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-
+  final String? location;
+  final String? flag;
+  final String? time;
+  final bool? isDayTime;
+  HomeScreen({this.flag, this.isDayTime, this.location, this.time});
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<HomeScreen> {
+  Object? data =
+      {}; // Empty map at the start to store data incoming from previous screen.
+  // The question mark means that we will assign value to the data variable identifier of type Object later on.
 
-  Object? data = {}; // Empty map at the start to store data incoming from previous screen.
-                     // The question mark means that we will assign value to the data variable identifier of type Object later on.
-
+  bool _newDataArrived = false;
+  Map newData = {};
   @override
   Widget build(BuildContext context) {
+    Map data = {
+      'location': widget.location,
+      'flag': widget.flag,
+      'time': widget.time,
+      'isDayTime': widget.isDayTime,
+    }; // Null value must be initiated to avoid error if nothing is extracted from http request.
 
-    Map data = {}; // Null value must be initiated to avoid error if nothing is extracted from http request.
-    data = data.isNotEmpty ? data : ModalRoute.of(context)!.settings.arguments as Map; // After choosing a location, the location screen pops off. The widget in home screen is not re-built. So, this function is not called
+    _newDataArrived ? data = newData : data = data;
+
+    // data.isNotEmpty
+    //     ? data
+    //     : ModalRoute.of(context)!.settings.arguments
+    //         as Map; // After choosing a location, the location screen pops off. The widget in home screen is not re-built. So, this function is not called
     print(data);
     // Not using setState here because this is the first time build function runs before returning any widget tree.
 
     // Set background
-    String bgImage = data['isDayTime'] ? 'pexels-darius-krause-2931915.jpg' : 'pexels-roberto-nickson-2885320.jpg';
+    String bgImage = data['isDayTime']
+        ? 'pexels-darius-krause-2931915.jpg'
+        : 'pexels-roberto-nickson-2885320.jpg';
     Color? bgColor = data['isDayTime'] ? Colors.blue[500] : Colors.blue[900];
     Color? textColor = data['isDayTime'] ? Colors.black : Colors.white;
 
@@ -41,25 +58,28 @@ class _HomeState extends State<HomeScreen> {
               children: <Widget>[
                 TextButton.icon(
                   onPressed: () async {
-                    dynamic result = await Navigator.pushNamed(context, 'location'); // Navigator is used to travel between multiple screens.
-                      if(result!=null){
-                        setState(() {
-                          data = {
-                            'time': result['time'],
-                            'location': result['location'],
-                            'flag': result['flag'],
-                            'isDayTime': result['isDayTime'],
-                          };
-                          print(data);
-                        });
-                      }
-                  },                                                                 // Push is used to push the new screen over existing screen.
-                  icon: Icon(                                                        // Named is used to provide the name of new screen.
+                    dynamic result = await Navigator.pushNamed(context,
+                        'location'); // Navigator is used to travel between multiple screens.
+                    if (result != null) {
+                      setState(() {
+                        _newDataArrived = true;
+                        newData = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'flag': result['flag'],
+                          'isDayTime': result['isDayTime'],
+                        };
+                        print(data);
+                      });
+                    }
+                  }, // Push is used to push the new screen over existing screen.
+                  icon: Icon(
+                    // Named is used to provide the name of new screen.
                     Icons.edit_location,
                     color: textColor,
                   ),
                   label: Text(
-                      "Edit Location",
+                    "Edit Location",
                     style: TextStyle(
                       color: textColor,
                     ),
--- lib/pages/loading_screen.dart
+++ lib/pages/loading_screen.dart
@@ -1,45 +1,61 @@
-import 'dart:js';
 import 'package:flutter/material.dart';
-import 'package:world_timer/pages/location_screen.dart';
 import 'package:world_timer/services/world_time.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'home_screen.dart';
 
 // First screen that loads up when app is started.
 class LoadingScreen extends StatelessWidget {
-
-  WorldTime instance = WorldTime(location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta');
+  final WorldTime instance = WorldTime(
+      location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta');
 
   Future<void> setupWorldTime(BuildContext context) async {
-    await instance.getTime();  // Await is used to finish this function before executing next operation.
-    print(instance.time); //This will not work because WorldTime class is asynchronous. It will take some time to bring data back from http request.
+    await instance
+        .getTime(); // Await is used to finish this function before executing next operation.
+    print(instance
+        .time); //This will not work because WorldTime class is asynchronous. It will take some time to bring data back from http request.
   }
 
   @override
   Widget build(BuildContext context) {
-    setupWorldTime(context); // This function will run as soon as the widget loads up.
-    return FutureBuilder(builder: (context, ss){
-      if(ss.connectionState==ConnectionState.waiting)
-        return Scaffold(
-        backgroundColor: Colors.blue[800],
-        body: Center(
-          child: SpinKitFadingCircle(
-            color: Colors.white,
-            size: 150.0,
-          ),
-        ),
-      );
-      else if(ss.connectionState==ConnectionState.done){
-        print(instance.location);
-        print(instance.flag);
+    // setupWorldTime(
+    //     context); // This function will run as soon as the widget loads up.
+    return FutureBuilder(
+        builder: (context, ss) {
+          if (ss.connectionState == ConnectionState.waiting)
+            Scaffold(
+              backgroundColor: Colors.blue[800],
+              body: Center(
+                child: SpinKitFadingCircle(
+                  color: Colors.white,
+                  size: 150.0,
+                ),
+              ),
+            );
+          else if (ss.connectionState == ConnectionState.done)
+            return HomeScreen(
+              flag: instance.flag,
+              isDayTime: instance.isDayTime,
+              location: instance.location,
+              time: instance.time,
+            );
 
-        Navigator.pushReplacementNamed(context, 'home', arguments: {  // pushReplacementNamed is used to replace the route beneath the current route because it will be no longer used.
-          'location': instance.location,  // arguments contains the data which is to be passed onto the next screen
-          'flag': instance.flag,
-          'time': instance.time,
-          'isDayTime': instance.isDayTime,
-        });}
-    }
-     ,future: setupWorldTime(context));
+          return Container(
+            color: Color(0xFFffe8d6),
+            child: Center(
+              child: Text(
+                'Fetching data...',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 16,
+                    color: Colors.black),
+              ),
+            ),
+          );
+          // {
+          // print(instance.location);
+          // print(instance.flag);
+          // }
+        },
+        future: setupWorldTime(context));
+  }
 }
-    }
-
